ad6ba579ab691cecc367723ea79538b0bcd7f2f3
IvanNasaktan <belaroivan17@gmail.com>
2025-07-03 02:23:11 +0800

    deployment bugs

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GradesController;
 use App\Http\Controllers\MetricsController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +30,12 @@ Route::middleware([
 Route::post('/semesters', [GradesController::class, 'storeSemester'])->name('semesters.store');
 Route::post('/courses', [GradesController::class, 'storeCourse'])->name('courses.store');
 Route::get('/metrics', [MetricsController::class, 'index'])->name('metrics');
+
+
+
+Route::get('/clear-cache', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    return '✅ Laravel config, cache, and views cleared.';
+});
