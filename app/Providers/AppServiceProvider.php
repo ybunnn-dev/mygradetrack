5774bc3cbd00722129ea9250565c23b80307a222
Ybunnn_dev <belaroivan17@gmail.com>
2025-07-06 01:07:48 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,30 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Add this line to import the URL facade
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
         //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // Add the following conditional check
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
