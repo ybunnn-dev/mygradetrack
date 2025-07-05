abd294eee4f1590247b2c687988607b9c0f28369
Ybunnn_dev <belaroivan17@gmail.com>
2025-07-05 23:20:37 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Replace __PORT__ in nginx config with the value of $PORT from Railway
+sed -i "s/__PORT__/${PORT}/g" /etc/nginx/nginx.conf
+
 # Start PHP-FPM in the background
 php-fpm &
 
-# Start Nginx in the foreground.
-# Nginx needs to stay in the foreground for the Docker container to keep running.
-exec nginx -g "daemon off;"
+# Start Nginx in the foreground
+nginx -g "daemon off;"
